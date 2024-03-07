482eb922cd01c89f5cc660873800f04629a866ac
yas <105318763+yasmin07lynx@users.noreply.github.com>
2024-03-06 21:29:04 -0300

    ok

--- main.js
+++ main.js
@@ -1,15 +1,14 @@
-$("#change").click(function () {
-$("#change").html("SIM");
-$("#move").show();
-});
-
-$("#move").hover(function() {
-change();
-} );
-
-function change()
-{
-var i = Math.floor(Math.random()*500)+1;
-var j = Math.floor(Math.random()*100)+1;
-$("#move").css({top: j+ 'px', left: i+'px'});
-}
+$("#change").click(function() {
+    $("#change").html("SIM");
+    $("#move").show();
+  });
+  
+  $("#move").hover(function() {
+    change();
+  });
+  
+  function change() {
+    var i = Math.floor(Math.random()*500)+1;
+    var j = Math.floor(Math.random()*100)+1;
+    $("#move").css({top: j+'px',left: i+'px'});
+  }
